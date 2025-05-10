63219684c47e93bb0d178984517a37080ec95dfa
cis-ash <nice.tushkan@gmail.com>
2025-05-10 16:06:23 -0400

    change eye logic a bit

--- eye.gd
+++ eye.gd
@@ -8,6 +8,7 @@ enum EYE_TYPE {CALM, CRAZY, TIRED}
 @export_range(0.0, 1.0) var tiredness : float = 0.0
 @export var eye_state : EYE_TYPE
 @export var eyebrow_angle : float
+@export var eyeball_offset : Vector2
 
 @export var open_eye : Node2D
 @export var closed_eye : Node2D
@@ -16,6 +17,8 @@ enum EYE_TYPE {CALM, CRAZY, TIRED}
 @export var crazy_eye : Node2D
 @export var tired_eye : Node2D
 @export var eyebrow : Node2D
+@export var eyeball : Node2D
+@export var flip_eyebrow : bool
 
 func _process(delta: float) -> void:
 	update_eye()
@@ -28,5 +31,6 @@ func update_eye():
 	crazy_eye.visible = eye_state == EYE_TYPE.CRAZY
 	tired_eye.visible = eye_state == EYE_TYPE.TIRED
 	eyebag.modulate.a = tiredness
-	eyebrow.rotation = eyebrow_angle
+	eyebrow.rotation = eyebrow_angle * (-1.0 if flip_eyebrow else 1.0)
+	eyeball.position = eyeball_offset
 	pass
