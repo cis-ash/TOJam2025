4bef66cb8f1543b991050206c3dc385526d390a0
cis-ash <nice.tushkan@gmail.com>
2025-05-10 15:17:14 -0400

    eye changes

---
+++ eye.gd
@@ -0,0 +1,32 @@
+@tool
+extends Node2D
+class_name Eye
+
+enum EYE_TYPE {CALM, CRAZY, TIRED}
+
+@export var blinking : bool = false
+@export_range(0.0, 1.0) var tiredness : float = 0.0
+@export var eye_state : EYE_TYPE
+@export var eyebrow_angle : float
+
+@export var open_eye : Node2D
+@export var closed_eye : Node2D
+@export var eyebag : Node2D
+@export var calm_eye : Node2D
+@export var crazy_eye : Node2D
+@export var tired_eye : Node2D
+@export var eyebrow : Node2D
+
+func _process(delta: float) -> void:
+	update_eye()
+	pass
+
+func update_eye():
+	open_eye.visible = not blinking
+	closed_eye.visible = blinking
+	calm_eye.visible = eye_state == EYE_TYPE.CALM
+	crazy_eye.visible = eye_state == EYE_TYPE.CRAZY
+	tired_eye.visible = eye_state == EYE_TYPE.TIRED
+	eyebag.modulate.a = tiredness
+	eyebrow.rotation = eyebrow_angle
+	pass
